278722b428910291a0cac331ab635cb3fae1c44e
ijlal123 <ijlal.tanveer291@gmail.com>
2026-01-09 16:15:04 +0500

    fixed overflow bug with clipping

--- triangle.cpp
+++ triangle.cpp
@@ -232,7 +232,7 @@ void triangle_t::draw_textured_triangle(
     if (y2 - y0 != 0) inv_slope_2 = (float)(x2 - x0) / abs(y2 - y0);
 
     if (y1 - y0 != 0) {
-        for (int y = y0; y < y1; y++) {
+        for (int y = y0; y <= y1; y++) {
             int x_start = x1 + (y - y1) * inv_slope_1;
             int x_end = x0 + (y - y0) * inv_slope_2;
 
@@ -257,7 +257,7 @@ void triangle_t::draw_textured_triangle(
     if (y2 - y0 != 0) inv_slope_2 = (float)(x2 - x0) / abs(y2 - y0);
 
     if (y2 - y1 != 0) {
-        for (int y = y1; y < y2; y++) {
+        for (int y = y1; y <= y2; y++) {
             int x_start = x1 + (y - y1) * inv_slope_1;
             int x_end = x0 + (y - y0) * inv_slope_2;
 
